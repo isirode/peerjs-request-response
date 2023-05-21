cd0a96d9348d086b61b6036cf819a6c0b1bdfa2c
isirode <isirode@outlook.com>
2023-05-21 19:42:42 +0200

    - typing the mapper
    - modifying the interface of IServer

--- src/domains/server/IServer.ts
+++ src/domains/server/IServer.ts
@@ -1,4 +1,9 @@
+import { IServerHandler } from "./port/IServerHandler";
+import { IServerMapper } from "./port/IServerMapper";
+
 export interface IServer {
+  handler: IServerHandler;
+  mapper: IServerMapper;
   listen(): void;
   stop(): void;
 }
--- src/domains/server/port/IServerMapper.ts
+++ src/domains/server/port/IServerMapper.ts
@@ -1,7 +1,7 @@
 import { Request } from '../../model/Request';
 import { Response } from '../../model/Response';
 
-export interface IServerMapper {
-  map(data: any): Request | undefined;
-  wrap(request: Response): any;
+export interface IServerMapper<T = unknown, U = unknown> {
+  map(data: T): Request | undefined;
+  wrap(response: Response): T;
 }
